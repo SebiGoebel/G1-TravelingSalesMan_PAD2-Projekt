4926606c83d99446447a5216b12a7f212f864c20
SebiGoebel <sebastian.goebel10@gmail.com>
2020-05-20 17:14:00 +0200

    updated main.c file

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "header.h"
+#include "functions.c"
+
 int main()
 {
     printf("G1-TravelingSalesManProblem\n");
+
+/*  Test:
+    int x = addieren(2,3);
+    printf("%d + %d = %d", 2, 3, x);
+    return x;
+*/
     return 0;
 }
