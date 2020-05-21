bbaf35cc80facdd766f3c0b6307cc4f276a8163d
SebiGoebel <sebastian.goebel10@gmail.com>
2020-05-21 16:43:10 +0200

    added header.h file and functions.c file

---
+++ header.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+
+//int addieren(int a, int b);
+
+void einlesenCSV();
+void loeschenCSV();
+
+void auslesenStaedtenamen();
+void auslesenPopulation();
+
+void sortierenStaedtenamen();
+void sortierenPopulation();
+
+
+#endif // HEADER_H_INCLUDED
