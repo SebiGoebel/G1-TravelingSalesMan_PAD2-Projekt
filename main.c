d5cb9e86a75d62ba61a4701ea7c62d16f301c90f
peterpallnstorfer <mr19b043@technikum-wien.at>
2020-05-21 17:40:42 +0200

    updated main.o/main.c/exe file

--- main.c
+++ main.c
@@ -4,10 +4,29 @@
 #include "header.h"
 #include "functions.c"
 
+
 int main()
 {
     int a=0;
+    char name[180];
     printf("G1-TravelingSalesManProblem\n");
+
+
+    FILE *myfile;
+    char *filename="worldcities.csv";
+    myfile=fopen(filename, "r");
+
+    if (NULL == myfile){
+        printf("could not open %s",filename);
+        exit(-1);
+    }
+    while(EOF!=fscanf(myfile,"%s", name))
+    {
+        printf("%s\n", name);
+    }
+
+    fclose(myfile);
+
     scanf("%d", &a);
 /*  Test:
     int x = addieren(2,3);
