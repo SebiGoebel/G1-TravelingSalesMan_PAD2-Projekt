c417bec94cff61572b9877d47868a27ec03de091
c0rkscr3w <rahil.chopra.rc@gmail.com>
2020-05-22 16:07:03 +0200

    added menue to main.c

--- main.c
+++ main.c
@@ -7,7 +7,8 @@
 
 int main()
 {
-    int a=0;
+    int x=0;
+    int y=0;
     char name[180];
     printf("G1-TravelingSalesManProblem\n");
 
@@ -20,18 +21,56 @@ int main()
         printf("could not open %s",filename);
         exit(-1);
     }
-    while(EOF!=fscanf(myfile,"%s", name))
+//    while(EOF!=fscanf(myfile,"%s", name))
+//    {
+//        printf("%s\n", name);
+//    }
+
+    //Hauptmenue
+    do
     {
-        printf("%s\n", name);
-    }
+      printf("0-- Programm beenden\n");
+      printf("1-- \n");
+      printf("2-- \n");
+      printf("3-- \n");
+      printf("4-- \n");
+      printf("5-- \n");
+
+      printf("Ihre Eingabe:");
+      scanf("%d", &x);
+
+      switch(x)
+      {
+        case 1:
+            printf("1\n");
+            break;
+        case 2:
+            printf("2\n");
+            break;
+        case 3:
+            printf("3\n");
+            break;
+        case 4:
+            printf("4\n");
+            break;
+        case 5:
+            printf("5\n");
+            break;
+        case 0:
+            return 0;
+            break;
+
+      }
+
+    }while(y == 0);
 
     fclose(myfile);
 
-    scanf("%d", &a);
+
 /*  Test:
     int x = addieren(2,3);
     printf("%d + %d = %d", 2, 3, x);
     return x;
 */
-    return 0;
+
 }
