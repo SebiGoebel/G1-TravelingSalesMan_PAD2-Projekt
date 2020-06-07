b0555c26daf3ff0260a6244d173375ba2adaafee
SebiGoebel <sebastian.goebel10@gmail.com>
2020-06-07 17:36:40 +0200

    added tirel-function and ascii art

--- Project/main.c
+++ Project/main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
+void titel();
+
 int main() {
-    printf("G1_TravellingSalesMan\n");
+    titel();
     return 0;
+}
+
+void titel()
+{
+    printf("  _[]_\n");
+    printf(" <(^_^)>\n");
+    printf("  ([])\n");
+    printf("  / |\n");
+    printf("--------------------------------\n");
+    printf("   _________                ____\n");
+    printf("  /                        // ||\n");
+    printf(" /                        //  ||\n");
+    printf("/                        //   ||\n");
+    printf("|                             ||\n");
+    printf("|                             ||\n");
+    printf("|                             ||\n");
+    printf(" \\       ___                  ||\n");
+    printf("  \\        |                  ||\n");
+    printf("   \\       |                  ||\n");
+    printf("    --------                 ----\n");
+    printf("---------------------------------\n");
+    printf("G1_TravellingSalesMan\n");
+    printf("=================================\n");
 }
