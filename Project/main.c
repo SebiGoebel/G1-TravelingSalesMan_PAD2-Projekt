936d1f0e49380dbdb2b59ab6132ed80a953e7fd5
SebiGoebel <sebastian.goebel10@gmail.com>
2020-06-07 18:14:45 +0200

    added menu to main

--- Project/main.c
+++ Project/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define LEN 100
 
 void titel();
 
@@ -15,10 +18,50 @@ typedef struct City{
 }City;
 
 int main() {
+
+    //title und ascii-art
     titel();
+
+    //Menu
+    //@Sebi
+    int menu_wahl;
+
+    do{
+        printf("MENU:\n");
+        printf("----------------------------\n");
+        printf("Bitte treffen Sie Ihre wahl:\n");
+        printf(" - 1 - Auswahl 1\n");
+        printf(" - 2 - Auswahl 2\n");
+        printf(" - 3 - Auswahl 3\n");
+        printf(" - 0 - Beenden\n");
+        printf("----------------------------\n");
+
+        //menu_wahl-Eingabe
+        scanf(" %d", &menu_wahl);
+
+        switch (menu_wahl) {
+            case 0:
+                printf("Progamm wird beendet\n\n");
+                break;
+            case 1:
+                printf("Sie haben die Wahl 1 getroffen\n");
+                break;
+            case 2:
+                printf("Sie haben die Wahl 2 getroffen\n");
+                break;
+            case 3:
+                printf("Sie haben die Wahl 3 getroffen\n");
+                break;
+            default:
+                printf("Keine gueltige Auswahl\n");
+        }
+    }while (menu_wahl != 0);
+
     return 0;
 }
 
+//titel und ascii-art
+//Sebi
 void titel()
 {
     printf("  _[]_\n");
