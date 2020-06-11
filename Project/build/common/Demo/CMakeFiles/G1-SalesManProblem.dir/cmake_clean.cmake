26ee01b8aa9d98a78d4052cb68422429a9ff0be2
SebiGoebel <sebastian.goebel10@gmail.com>
2020-06-11 11:37:05 +0200

    created build folder, created updated exe, --> everything works yeay

---
+++ Project/build/common/Demo/CMakeFiles/G1-SalesManProblem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../../bin/G1-SalesManProblem.exe"
+  "../../bin/G1-SalesManProblem.exe.manifest"
+  "../../bin/G1-SalesManProblem.pdb"
+  "../../lib/libG1-SalesManProblem.dll.a"
+  "CMakeFiles/G1-SalesManProblem.dir/main.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/G1-SalesManProblem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
